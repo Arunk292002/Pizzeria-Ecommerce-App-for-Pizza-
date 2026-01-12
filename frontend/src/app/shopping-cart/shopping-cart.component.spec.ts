88b759b8600bec724f3ebc2b93d93223e21625a0
Arun Kumar <111553611+Arunk292002@users.noreply.github.com>
2026-01-12 15:55:41 +0530

    Update shopping-cart.component.spec.ts

--- frontend/src/app/shopping-cart/shopping-cart.component.spec.ts
+++ frontend/src/app/shopping-cart/shopping-cart.component.spec.ts
@@ -18,4 +18,31 @@ describe("ShoppingCartComponent", () => {
   it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("should calculate total cost correctly", () => {
+  component.igList = [
+    { name:'Cheese',price: 200 },
+    { name:'Black Olives',price: 300 },
+  ];
+  component.igCost();
+  expect(component.ingredientsCost).toBe(500);
+});
+it("should display NaN", () => {
+  component.pzaList = [
+    { price: 200, qty:1 },
+    { price: 300, qty:1 },
+  ];
+
+  component.pzaCost();
+  expect(component.pizzaCost).toBe(500);
+});
+it('should remove from cart', ()=>{
+  component.pzaList = [
+    { name:'Cheese Pizza', price: 200, qty:1 },
+    { name: 'Panneer Pizza', price: 300, qty:1 },
+  ];
+  component.clearCart();
+  expect(component.pzaList).toEqual([]);
+})
+
 });
